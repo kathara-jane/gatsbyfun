4d89862f443427632b84fa675d18c11821dd36b3
Katherine Sanborn <katherine.j.sanborn@gmail.com>
2021-03-03 13:18:14 -0500

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+  <>
+  <h1>About</h1>
+  <h2>Name: Katherine Sanborn</h2>
+  <h2>Job: Software Engineer</h2>
+  </>
+  )
+}
+
+export default About
